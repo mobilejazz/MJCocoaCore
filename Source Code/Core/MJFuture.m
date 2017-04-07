2ad5e513978b957ae7f2f163ba2a7011e5431cfc
Jose Luis  Franconetti Olmedo <joseluis@mobilejazz.com>
2017-04-07 15:15:16 +0200

    Fixed issue when the future value is nil.

--- Source Code/Core/MJFuture.m
+++ Source Code/Core/MJFuture.m
@@ -16,16 +16,14 @@
 
 #import "MJFuture.h"
 
-NSString * const MJFutureValueNotAvailableException = @"MJFutureValueNotAvailableException";
-NSString * const MJFutureErrorKey = @"MJFutureErrorKey";
+NSString *const MJFutureValueNotAvailableException = @"MJFutureValueNotAvailableException";
+NSString *const MJFutureErrorKey = @"MJFutureErrorKey";
 
 #define MJFutureDuplicateInvocationException(method) [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"MJFuture doesn't allow calling twice the method <%@>.", NSStringFromSelector(@selector(method))] userInfo:nil]
 
 #define MJFutureAlreadySentException [NSException exceptionWithName:NSInternalInconsistencyException reason:@"Future already sent." userInfo:nil]
 
-static dispatch_queue_t defaultReturnQueue = nil;
-
-@interface MJFuture()
+@interface MJFuture ()
 
 @property (nonatomic, copy, readwrite) void (^thenBlock)(id, NSError *);
 
@@ -33,130 +31,127 @@ static dispatch_queue_t defaultReturnQueue = nil;
 
 @implementation MJFuture
 {
-	id _value;
-	id _error;
-	
-	dispatch_queue_t _customQueue;
-    
-    dispatch_semaphore_t _semaphore;
-	
-	NSHashTable <id <MJFutureObserver>> *_observers;
-}
+    id _value;
+    id _error;
+    BOOL _isValueNil;
 
-+ (void)setDefaultReturnQueue:(dispatch_queue_t)queue
-{
-    defaultReturnQueue = queue;
+    dispatch_queue_t _customQueue;
+
+    dispatch_semaphore_t _semaphore;
+
+    NSHashTable <id <MJFutureObserver>> *_observers;
 }
 
 + (MJFuture *)emptyFuture
 {
-	MJFuture *future = [[MJFuture alloc] init];
-	return future;
+    MJFuture *future = [[MJFuture alloc] init];
+    return future;
 }
 
-+ (MJFuture*)immediateFuture:(id)value
++ (MJFuture *)immediateFuture:(id)value
 {
-	MJFuture *future = [[MJFuture alloc] init];
-	[future setValue:value];
-	return future;
+    MJFuture *future = [[MJFuture alloc] init];
+    [future setValue:value];
+    return future;
 }
 
 - (instancetype)init
 {
-	self = [super init];
-	if (self)
-	{
-		_state = MJFutureStateBlank;
-		_observers = [NSHashTable hashTableWithOptions:NSPointerFunctionsWeakMemory];
-        
-        if (defaultReturnQueue)
-        {
-            _returnQueue = defaultReturnQueue;
-        }
-        else
-        {
-            _returnQueue = dispatch_get_main_queue();
-        }
-        
-	}
-	return self;
+    self = [super init];
+    if (self)
+    {
+        _state = MJFutureStateBlank;
+        _returnQueue = dispatch_get_main_queue();
+        _observers = [NSHashTable hashTableWithOptions:NSPointerFunctionsWeakMemory];
+    }
+    return self;
 }
 
 - (void)setValue:(id)value
 {
-	@synchronized (self)
-	{
-		if (_value)
-		{
-			[MJFutureDuplicateInvocationException(setValue:) raise];
-		}
-		
-		_value = value;
-		
-		[_observers.allObjects enumerateObjectsUsingBlock:^(id <MJFutureObserver> _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-			if ([obj respondsToSelector:@selector(future:didSetValue:)])
-			{
-				[obj future:self didSetValue:value];
-			}
-		}];
-		
-		[self mjz_update];
-	}
+    @synchronized (self)
+    {
+        if (value == nil)
+            _isValueNil = YES;
+
+        if (_value)
+        {
+            [MJFutureDuplicateInvocationException(setValue:) raise];
+        }
+
+        _value = value;
+
+        [_observers.allObjects enumerateObjectsUsingBlock:^(id <MJFutureObserver> _Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+            if ([obj respondsToSelector:@selector(future:didSetValue:)])
+            {
+                [obj future:self didSetValue:value];
+            }
+        }];
+
+        [self mjz_update];
+    }
 }
 
 - (void)setError:(NSError *)error
 {
-	@synchronized (self)
-	{
-		if (_error)
-		{
-			[MJFutureDuplicateInvocationException(setError:) raise];
-		}
-		
-		_error = error;
-		
-		[_observers.allObjects enumerateObjectsUsingBlock:^(id <MJFutureObserver> _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-			if ([obj respondsToSelector:@selector(future:didSetError:)])
-			{
-				[obj future:self didSetError:error];
-			}
-		}];
-		
-		[self mjz_update];
-	}
+    @synchronized (self)
+    {
+        if (_error)
+        {
+            [MJFutureDuplicateInvocationException(setError:) raise];
+        }
+
+        if (error)
+        {
+            _error = error;
+
+            [_observers.allObjects enumerateObjectsUsingBlock:^(id <MJFutureObserver> _Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+                if ([obj respondsToSelector:@selector(future:didSetError:)])
+                {
+                    [obj future:self didSetError:error];
+                }
+            }];
+
+            [self mjz_update];
+        }
+        else
+        {
+            NSLog(@"WARNING: Error set in MJFuture with nil value. This is a warning message, nothing is going to happen.");
+        }
+    }
 }
 
 - (void)wontHappen
 {
-	_state = MJFutureStateWontHappen;
-	
-	[_observers.allObjects enumerateObjectsUsingBlock:^(id <MJFutureObserver> _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-		if ([obj respondsToSelector:@selector(wontHappenFuture:)])
-		{
-			[obj wontHappenFuture:self];
-		}
-	}];
+    _state = MJFutureStateWontHappen;
+
+    [_observers.allObjects enumerateObjectsUsingBlock:^(id <MJFutureObserver> _Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+        if ([obj respondsToSelector:@selector(wontHappenFuture:)])
+        {
+            [obj wontHappenFuture:self];
+        }
+    }];
 }
 
 - (void)then:(void (^)(id, NSError *))block
 {
-	[self then:block inQueue:nil];
+    [self then:block inQueue:nil];
 }
 
 - (void)then:(void (^)(id, NSError *))block inQueue:(dispatch_queue_t)queue
 {
-	@synchronized (self)
-	{
-		if (_thenBlock)
-		{
-			[MJFutureDuplicateInvocationException(then:) raise];
-		}
-		
-		self.thenBlock = block;
-		_customQueue = queue;
-		
-		[self mjz_update];
-	}
+    @synchronized (self)
+    {
+        if (_thenBlock)
+        {
+            [MJFutureDuplicateInvocationException(then:) raise];
+        }
+
+        self.thenBlock = block;
+        _customQueue = queue;
+
+        [self mjz_update];
+    }
 }
 
 - (_Nullable id)value
@@ -184,7 +179,7 @@ static dispatch_queue_t defaultReturnQueue = nil;
     {
         _semaphore = dispatch_semaphore_create(0);
         dispatch_semaphore_wait(_semaphore, DISPATCH_TIME_FOREVER);
-        
+
         return [self value];
     }
     else
@@ -194,119 +189,119 @@ static dispatch_queue_t defaultReturnQueue = nil;
                                                        userInfo:nil];
         @throw exception;
     }
-    
+
     return nil;
 }
 
 - (void)addObserver:(id <MJFutureObserver>)observer
 {
-	[_observers addObject:observer];
-	
-	if (_state == MJFutureStateSent || _state == MJFutureStateWaitingBlock)
-	{
-		if (_value)
-		{
-			if ([observer respondsToSelector:@selector(future:didSetValue:)])
-			{
-				[observer future:self didSetValue:_value];
-			}
-		}
-		else if (_error)
-		{
-			if ([observer respondsToSelector:@selector(future:didSetError:)])
-			{
-				[observer future:self didSetError:_error];
-			}
-		}
-	}
-	else if (_state == MJFutureStateWontHappen)
-	{
-		if ([observer respondsToSelector:@selector(wontHappenFuture:)])
-		{
-			[observer wontHappenFuture:self];
-		}
-	}
+    [_observers addObject:observer];
+
+    if (_state == MJFutureStateSent || _state == MJFutureStateWaitingBlock)
+    {
+        if (_value)
+        {
+            if ([observer respondsToSelector:@selector(future:didSetValue:)])
+            {
+                [observer future:self didSetValue:_value];
+            }
+        }
+        else if (_error)
+        {
+            if ([observer respondsToSelector:@selector(future:didSetError:)])
+            {
+                [observer future:self didSetError:_error];
+            }
+        }
+    }
+    else if (_state == MJFutureStateWontHappen)
+    {
+        if ([observer respondsToSelector:@selector(wontHappenFuture:)])
+        {
+            [observer wontHappenFuture:self];
+        }
+    }
 }
 
 - (void)removeObserver:(id <MJFutureObserver>)observer
 {
-	[_observers removeObject:observer];
+    [_observers removeObject:observer];
 }
 
 #pragma mark Private Methods
 
 - (void)mjz_update
 {
-	if (_state == MJFutureStateSent)
-	{
-		[MJFutureAlreadySentException raise];
-	}
-	else if (_state == MJFutureStateBlank)
-	{
-		// Waiting for either value||error , or the then block.
-		
-		if (_value || _error)
-		{
-			_state = MJFutureStateWaitingBlock;
-            
+    if (_state == MJFutureStateSent)
+    {
+        [MJFutureAlreadySentException raise];
+    }
+    else if (_state == MJFutureStateBlank)
+    {
+        // Waiting for either value||error , or the then block.
+
+        if (_value || _error)
+        {
+            _state = MJFutureStateWaitingBlock;
+
             if (_semaphore != nil)
             {
                 dispatch_semaphore_signal(_semaphore);
             }
-		}
-		else if (_thenBlock)
-		{
-			_state = MJFutureStateWaitingValueOrError;
-		}
-	}
-	else if (_state == MJFutureStateWaitingBlock)
-	{
-		if	(_thenBlock)
-		{
-			[self mjz_send];
-			_state = MJFutureStateSent;
-		}
-	}
-	else if (_state == MJFutureStateWaitingValueOrError)
-	{
-		if (_value || _error)
-		{
-			[self mjz_send];
-			_state = MJFutureStateSent;
-		}
-	}
+        }
+        else if (_thenBlock)
+        {
+            _state = MJFutureStateWaitingValueOrError;
+        }
+    }
+    else if (_state == MJFutureStateWaitingBlock)
+    {
+        if (_thenBlock)
+        {
+            [self mjz_send];
+            _state = MJFutureStateSent;
+        }
+    }
+    else if (_state == MJFutureStateWaitingValueOrError)
+    {
+        if ((_value || _isValueNil) || _error)
+        {
+            [self mjz_send];
+            _state = MJFutureStateSent;
+        }
+    }
 }
 
 - (void)mjz_send
 {
-	void (^thenBlock)(id, NSError *) = _thenBlock;
-	id value = _value;
-	id error = _error;
-	
-	if (_customQueue)
-	{
-		dispatch_async(_customQueue, ^{
-			thenBlock(value, error);
-		});
-	}
-	else
-	{
-		dispatch_async(_returnQueue, ^{
-			thenBlock(value, error);
-		});
-	}
-	
-	[_observers.allObjects enumerateObjectsUsingBlock:^(id <MJFutureObserver> _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-		if ([obj respondsToSelector:@selector(didSendFuture:)])
-		{
-			[obj didSendFuture:self];
-		}
-	}];
-	
-	self.thenBlock = nil;
-	_value = nil;
-	_error = nil;
-	_customQueue = nil;
+    void (^thenBlock)(id, NSError *) = _thenBlock;
+    id value = _value;
+    id error = _error;
+
+    if (_customQueue)
+    {
+        dispatch_async(_customQueue, ^{
+            thenBlock(value, error);
+        });
+    }
+    else
+    {
+        dispatch_async(_returnQueue, ^{
+            thenBlock(value, error);
+        });
+    }
+
+    [_observers.allObjects enumerateObjectsUsingBlock:^(id <MJFutureObserver> _Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+        if ([obj respondsToSelector:@selector(didSendFuture:)])
+        {
+            [obj didSendFuture:self];
+        }
+    }];
+
+    self.thenBlock = nil;
+    _value = nil;
+    _error = nil;
+    _customQueue = nil;
 }
 
 @end
