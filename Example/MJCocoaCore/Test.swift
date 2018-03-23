fdff4f3085d8410ad549a12bfb834f13bc4fe1b5
Joan Martin <joan@mobilejazz.com>
2018-03-23 08:14:53 +0100

    Improving swift interface of MJFuture

---
+++ Example/MJCocoaCore/Test.swift
@@ -0,0 +1,23 @@
+//
+//  Test.swift
+//  MJCocoaCore_Example
+//
+//  Created by Joan Martin on 23/03/2018.
+//  Copyright © 2018 Joan Martin. All rights reserved.
+//
+
+import Foundation
+import MJCocoaCore
+
+func testFuture() {
+    
+    let future = Future<NSString>()
+    
+    future.then { value in
+        
+        }.fail { error in
+            
+    }
+    
+}
+
